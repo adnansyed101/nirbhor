e23e08c1ba44b092f52267e0fd4dcad19688f9ec
Ashis Kumar Joy <ashis263@gmail.com>
2025-03-30 23:38:15 +0600

    refactor: modified to arraow function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-export default function Home() {
+const Home = () => {
   return (
-    <>
+    <div>
       <h1>Welcome to Nirbhor</h1>
       <p>Your comfort, our commitment.</p>
-    </>
+    </div>
   );
 }
+
+export default Home;
