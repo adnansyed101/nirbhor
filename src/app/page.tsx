8cc0c3318c575221524c007d30aa49259d286a0d
jubayer <jubayerahmed.dev@gmail.com>
2025-03-30 23:52:28 +0600

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,10 @@ const Home = () => {
   return (
     <div>
       <h1>Welcome to Nirbhor</h1>
-<<<<<<< HEAD
       <p>Ready for conflict</p>
       <p>Your comfort our commitment.</p>
-    </>
-=======
-      <p>Your comfort, our commitment.</p>
     </div>
->>>>>>> e23e08c1ba44b092f52267e0fd4dcad19688f9ec
   );
-}
+};
 
 export default Home;
